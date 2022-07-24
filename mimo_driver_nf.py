98d5bf05e40b63b247163509fb3fac7039aaa337
ToddRenn <63478472+ToddRenn@users.noreply.github.com>
2022-07-24 10:27:04 -0600

    adding python nf tests

--- mimo_driver_nf.py
+++ mimo_driver_nf.py
@@ -71,7 +71,7 @@ class MIMODriver:
         [bs.close() for bs in self.bs_obj]
 
     def nf(self, n_frames, n_samps):
-        bs_sched="R"
+        bs_sched='GRRG'
         [bs.config_sdr_tdd() for i, bs in enumerate(self.bs_obj)] #just guard band - no tx or rx
         [bs.activate_stream_rx() for bs in self.bs_obj]
         rx_data = np.empty((n_frames, self.n_bs_antenna, n_samps), dtype=np.complex64)
@@ -101,19 +101,19 @@ class MIMODriver:
         rx_data_pwr = np.real(rx_data_rms)**2
         rx_data_pwr_dB = 10*np.log10(rx_data_pwr)
 
-        return [rx_data , rx_data_pwr_dB]
+        return rx_data , rx_data_pwr_dB
 
 #########################################
 #                  Main                 #
 #########################################
 def main():
     parser = OptionParser()
-    parser.add_option("--hub", type="string", dest="hub", help="serial number of the hub device", default="FH4B000019")
-    parser.add_option("--bs-serials", type="string", dest="bs_serials", help="serial numbers of the BS devices", default='RF3E000146,RF3E000356,RF3E000546')
-    parser.add_option("--ue-serials", type="string", dest="ue_serials", help="serial numbers of the UE devices", default='RF3D000016')
+    parser.add_option("--hub", type="string", dest="hub", help="serial number of the hub device", default='FH4B000003')
+    parser.add_option("--bs-serials", type="string", dest="bs_serials", help="serial numbers of the BS devices", default='RF3E000698,RF3E000731,RF3E000747,RF3E000734')
+    parser.add_option("--ue-serials", type="string", dest="ue_serials", help="serial numbers of the UE devices", default=[])
     parser.add_option("--rate", type="float", dest="rate", help="Tx sample rate", default=5e6)
-    parser.add_option("--freq", type="float", dest="freq", help="Tx freq (Hz). POWDER users must set to 3.6e9", default=3.6e9)
-    parser.add_option("--tx-gain", type="float", dest="tx_gain", help="Optional Tx gain (dB)", default=81.0)
+    parser.add_option("--freq", type="float", dest="freq", help="Tx freq (Hz). POWDER users must set to 3.6e9", default=3.547e9)
+    parser.add_option("--tx-gain", type="float", dest="tx_gain", help="Optional Tx gain (dB)", default=0)
     parser.add_option("--rx-gain", type="float", dest="rx_gain", help="Optional Rx gain (dB)", default=60.0)
     (options, args) = parser.parse_args()
     mimo = MIMODriver(
@@ -126,44 +126,12 @@ def main():
         tx_gain=options.tx_gain,
         rx_gain=options.rx_gain
     )
-    nsamps = 1024
-    nsamps_pad = 82
-    n_sym_samp = nsamps + 2*nsamps_pad - 14
 
-    ltsSym, lts_f = gen_lts(cp=32, upsample=1)
+    test_nf = True
+    if test_nf:
+        rx_vec_iris, nf_pwr_dB = mimo.nf(1, 4096)
+        print("rx_vec_iris.shape: {}, nf_pwr_dB: {}".format(rx_vec_iris.shape, nf_pwr_dB))
 
-    # to comprensate for front-end group delay
-    pad1 = np.zeros((nsamps_pad), np.complex64)
-    # to comprensate for rf path delay
-    pad2 = np.zeros((nsamps_pad-14), np.complex64)
-
-    wb_pilot = np.tile(ltsSym, nsamps//len(ltsSym)).astype(np.complex64)*.5
-    wbz = np.zeros((n_sym_samp), dtype=np.complex64)
-    wb_pilot1 = np.concatenate([pad1, wb_pilot, pad2])
-    wb_pilot1 = np.transpose(np.matlib.repmat(wb_pilot1, len(options.ue_serials.split(',')), 1))
-    wb_pilot2 = wbz  # wb_pilot1 if both_channels else wbz
-
-    test_uplink = True
-    if test_uplink:
-        ul_rx_data, n_ul_good, numRxSyms = mimo.txrx_uplink(np.real(wb_pilot1), np.imag(wb_pilot1), 5, len(pad2))
-        print("Uplink Rx Num {}, ShapeRxData: {}, NumRsyms: {}".format(n_ul_good, ul_rx_data.shape, numRxSyms))
-
-    test_downlink = False
-    if test_downlink:
-        dl_rx_data, n_dl_good, numRxSyms = mimo.txrx_downlink(np.real(wb_pilot1), np.imag(wb_pilot1), 1, len(pad2))
-        print("Downlink Rx Num {}".format(n_dl_good))
-
-    test_sounding = False
-    if test_sounding:
-        snd_rx_data, n_snd_good, numRxSyms = mimo.txrx_dl_sound(np.real(wb_pilot1), np.imag(wb_pilot1), 1, len(pad2))
-        print("Sounding (Downlink) Rx Num {}".format(n_snd_good))
-
-    # 10 frames
-    #[txg, rxg, max_val, is_valid] = mimo.set_opt_gains(10)
-    #print("BEST GAIN COMBO: TX {} / RX {} with MAX # BEACONS: {}".format(txg,rxg,max_val))
-    # Test gain change
-    #mimo.update_sdr_params('txgain', 30)
-    #mimo.update_sdr_params('rxgain', 30)
     mimo.close()
 
 if __name__ == '__main__':
